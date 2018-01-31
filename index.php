9fdb962081ce5492966a2757572f0e2bc0b9d0da
吉田聡 <yos.3104@gmail.com>
2018-01-31 22:24:47 +0900

    Import DataTables. change Bootstrap version.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $dirPath = './images';
 $listResult = '';
 if (is_readable($dirPath) && is_dir($dirPath)) {
 $listResult = '';
-$listResult = '<table class="table table-bordered">'
+$listResult = '<table class="table table-bordered" id="icons-table">'
         .'<thead>'
         .'<tr><th>Image</th><th>File name</th><th>Image size(WxH)</th><th>File size</th></tr>'
         .'</thead>'
@@ -16,7 +16,7 @@ $listResult = '<table class="table table-bordered">'
             $imageInfo = getimagesize($fullPath);
             $fileByteSize = filesize($fullPath);
             $listResult .= '<tr>';
-            $listResult .= '<td class="thumbnail"><a href="' . $fullPath . '" download="' . $val . '"><img src = "' . $fullPath . '" alt="' . $val . '"></td>';
+            $listResult .= '<td class="icon-image"><a href="' . $fullPath . '" download="' . $val . '"><img src = "' . $fullPath . '" alt="' . $val . '"></td>';
             $listResult .= '<td class="file-name">'. $val . '</td>';
             $listResult .= '<td class="image-size">'. $imageInfo[0] . ' x ' . $imageInfo[1] . '</td>';
             $listResult .= '<td class="file-size">'. number_format($fileByteSize / 1024, 2) . 'KB</td>';
@@ -34,11 +34,36 @@ $listResult = '<table class="table table-bordered">'
     <head>
         <meta charset="utf-8">
         <title>画像一覧</title>
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css" rel="stylesheet">
+        <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
         <style>
             body {
                 padding: 10px;
             }
+            .icon-image img {
+                width: 50px;
+                height: auto;
+            }
+            .icon-image {
+                background-color: #F5F5F5;
+            }
+            .icon-image a {
+                display: block;
+                text-align: center;
+            }
+            .dataTables_length,
+            .dataTables_info,
+            .dataTables_paginate.paging_simple_numbers {
+                display: none;
+            }
+            th:hover {
+                background-color: #F5F5F5;
+            }
+
+            /* Bootstrap style overwrite */
             .table {
                 width: auto;
             }
@@ -49,17 +74,32 @@ $listResult = '<table class="table table-bordered">'
             }
             .table td,
             .table th {
-                vertical-align: middle; 
                 padding: .4rem;
             }
-            .thumbnail img {
-                width: 50px;
-                height: auto;
+
+            /* DataTable style overwrite */
+            div.dataTables_wrapper div.dataTables_filter {
+                text-align: left;
             }
-            .thumbnail {
-                background-color: #F5F5F5;
+            div.dataTables_wrapper div.dataTables_paginate {
+                text-align: left;
+            }
+            .table>tbody>tr>td,
+            .table>tbody>tr>th,
+            .table>tfoot>tr>td,
+            .table>tfoot>tr>th,
+            .table>thead>tr>td,
+            .table>thead>tr>th {
+                vertical-align: middle;
             }
         </style>
+        <script>
+            $(document).ready(function() {
+                $('#icons-table').DataTable({
+                    "lengthMenu": [[-1], ["All"]]
+                });
+            });
+        </script>
     </head>
     <body>
         <?php
